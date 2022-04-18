e6838062c7f67a46615742e4c6ecfe4fbcd60ccb
zahramirkazemi <zahramirkazemi1997@gmail.com>
2022-04-18 19:43:01 +0430

    add react-notify

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from "react";
 import QuizCard from "./QuizCard";
 import Board from "./Board";
 import url from '../api';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [quiz, setQuiz] = useState([]);
@@ -19,10 +21,31 @@ function App() {
   }, []);
 
   const submitAnswer = (userAnswer) => {
-    if(questionNumber < questionCount){
+    if(questionNumber <= questionCount){
       if(quiz[questionNumber].answer === userAnswer){
+        toast.success(' True ! ', {
+          position: "bottom-right",
+          autoClose: 1200,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark"
+          });
         setScore(score+1);
-      }    
+      }
+      else{
+        toast.error(' False ! ', {
+          position: "bottom-right",
+          autoClose: 1200,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          });
+      }  
     }
     isLastQuestion();    
   }
@@ -32,12 +55,24 @@ function App() {
       setQuestionNumber(questionNumber+1);
     }    
     else{
-      alert(`you finished quiz with ${score} true answer of ${questionCount + 1} Qusetions!`)
+      //todo
     }
   }
 
   return (
     <React.Fragment>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={1200}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme={"dark"}
+      />
       <Board title="Question" number={questionNumber + 1} descrip={`of ${questionCount + 1}`} position="left"/>
       <QuizCard quiz={quiz} submitAnswer={submitAnswer} questionNumber={questionNumber}/>
       <Board title="Score" number={score} descrip="point" position="right"/>
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,5 @@
 const Board = ({ title, number, descrip, position}) => {
-    let style ="fixed top-2 rounded-lg p-4 bg-gray-900 text-center drop-shadow-xl"+ (position == "right"? " right-2": " left-2")
+    let style ="fixed top-2 rounded-lg p-4 bg-gray-900 text-center drop-shadow-xl"+ (position === "right"? " right-2": " left-2")
     return ( 
         <div className={style}>
             <p className="text-white font-sans font-semibold text-xs lg:text-lg md:text-base sm:text-xs">{title}</p>
