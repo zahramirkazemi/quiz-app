159589501238a78a763823bcb05a9104199e2102
zahramirkazemi <zahramirkazemi1997@gmail.com>
2022-04-19 22:21:54 +0430

    fix shadow bug

--- src/components/QuizCard.js
+++ src/components/QuizCard.js
@@ -5,7 +5,7 @@ import Options from "./Options";
 const QuizCard = ({quiz, questionNumber, submitAnswer}) => {
     
     return ( 
-        <div className="w-4/5 md:w-3/4 lg:w-1/2 rounded-lg mx-auto p-6 bg-gray-900 text-center flex flex-col gap-2 drop-shadow-xl">
+        <div className="w-4/5 md:w-3/4 lg:w-1/2 rounded-lg mx-auto p-6 bg-gray-900 text-center flex flex-col gap-2">
         {
           quiz[questionNumber] ? <Question question={quiz[questionNumber].question}/>: ''
         }
