5d1e855c297dbc88e72184ff30c1c2a6e0916ee1
zahra mirkazemi <94597090+zahramirkazemi@users.noreply.github.com>
2022-05-16 14:41:19 +0430

    update url

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://my-json-server.typicode.com/zahramirkazemi/quiz-api"
+    baseURL: "https://my-json-server.typicode.com/zahramirkazemi/projects-api"
 })
